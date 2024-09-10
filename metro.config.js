f2f1e004a05d21db6bcd18abadbe5db4aa547e69
Keith Kurak <keith.kurak@gmail.com>
2024-09-10 09:57:55 -0400

    remove native folders, adopt CNG

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,3 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = getDefaultConfig(__dirname);
